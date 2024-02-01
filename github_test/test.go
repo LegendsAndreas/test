8f635b0f6247633eab313a01a031e249f9d0d678
Andreas <aoch21@student.aau.dk>
2024-02-01 10:28:40 +0100

    Newest

--- github_test/test.go
+++ github_test/test.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b []int
+	a = make([]int, 4)
+	b = make([]int, 4)
+
+	a[0] = 1
+	a[1] = 2
+	a[2] = 3
+	a[3] = 4
+
+	b[0] = 1
+	b[1] = 2
+	b[2] = 2
+	b[3] = 2
+	fmt.Println(a, b)
+
+	var c []int
+	c = make([]int, 0)
+
+	c = mergeSlices(a, b)
+
+	fmt.Print(c)
+}
+
+func mergeSlices(s1 []int, s2 []int) []int {
+	var slice []int
+	slice = make([]int, 0)
+
+	for i := 0; i < ; i++ {
+		
+	}
+	
+	return slice
 }
